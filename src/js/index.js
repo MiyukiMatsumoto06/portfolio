ce1d31492c42eba0952839f62c1f25d35359fcff
Miyuki Matsumoto <matsumoto.miyuki0612@gmail.com>
2024-06-24 23:29:46 +0900

    20240624

--- src/js/index.js
+++ src/js/index.js
@@ -7,4 +7,21 @@ $(() => {
 
   const hoge = 'hoge'
   console.log(hoge)
+
+  //  ハンバーガーメニュー
+  const menuBtn = $('.c-menuBtn')
+  const nav = $('.p-nav')
+
+  menuBtn.click(function () {
+    $(this).toggleClass('is-active')
+    nav.toggleClass('is-active')
+  })
+
+  // Shop Listタブ開閉
+  const navBox = $('.p-nav__box')
+  navBox.click(function () {
+    const innerList = $(this).siblings('.p-nav__innerList')
+    $(this).toggleClass('is-active')
+    innerList.toggleClass('is-active')
+  })
 })
